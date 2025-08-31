1ebc96f12ebacfbc0beae6b891fdbe300b3b4424
PrompopKeawsod <42951458+PrompopKeawsod@users.noreply.github.com>
2025-08-31 15:03:26 +0700

    update lab2.py and agent.py

--- agent.py
+++ agent.py
@@ -2,16 +2,16 @@ from pygame.draw import circle, line, rect
 from pygame.math import Vector2
 
 class Agent:
-    def __init__(self):
-        self.color_red = (255,0,0)
-        self.circle_redius = 100
+    def __init__(self, position, redius, color):
+        self.color = color
+        self.circle_redius = redius
         self.vel = Vector2(0, 0)
-        self.position = Vector2(200, 200)
+        self.position = position
         self.acc = Vector2(0, 0)
         self.acc.x = 1
         self.acc.y = 1
 
-    def update(self):
+    def update(self, delta_time_ms):
         self.vel = self.vel + self.acc
 
         self.position = self.position + self.vel
@@ -20,4 +20,4 @@ class Agent:
         self.acc.y = 0
 
     def draw(self, screen):
-        circle(screen, self.color_red, self.position, self.circle_redius)
+        circle(screen, self.color, self.position, self.circle_redius)
--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.draw import circle, line, rect
 from pygame.math import Vector2
+from agent import Agent
 
 screen_width = 1280
 screen_height = 720
@@ -14,42 +15,31 @@ class App:
         
         self.running = True
 
-        self.color_red = (255,0,0)
-        self.circle_redius = 100
-        self.position = Vector2(screen_width//2, screen_height//2)
+        self.ball = Agent(position=Vector2(screen_width//2, screen_height//2), redius=100, color=(255,0,0)) #เรียกและส่งหน้าจอไปยังไฟล์ agent
 
-
-        self.vel = Vector2(0, 0)
-
-        self.acc = Vector2(0, 0)
-        self.acc.x = 1
-        self.acc.y = 1
+        
 
     def handle_input(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-    def update(self):
-        self.vel = self.vel + self.acc
-
-        self.position = self.position + self.vel
-
-        self.acc.x = 0
-        self.acc.y = 0
+    def update(self, delta_time_ms):
+        self.ball.update(delta_time_ms)
 
     def draw(self):
         self.screen.fill("grey")
-        circle(self.screen, self.color_red, self.position, self.circle_redius)
+        self.ball.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
-        while self.running:            
+        while self.running:        
+            delta_time_ms = self.clock.tick(60)    
             self.handle_input()
-            self.update()
+            self.update(delta_time_ms)
             self.draw()
 
-            self.clock.tick(60)
+            
 
 def main():
     app = App()
